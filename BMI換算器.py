7934aefa9a9b506b71e0f716151315665d313262
Eric Yang <xsw26845@gmail>
2022-06-24 09:26:42 +0800

    update BMI換算器

--- BMI換算器.py
+++ BMI換算器.py
@@ -4,4 +4,21 @@ weight = input('請輸入體重(公斤): ')
 height = float(height)
 weight = float(weight)
 BMI = weight / (height * height)
-print('您的BMI為', BMI)
+print('你的BMI為', BMI)
+if BMI < 18.5:
+	print('你的體重太輕')
+	print('需要多加注意')
+elif BMI >= 18.5 and BMI < 24:
+	print('你的體重剛剛好')
+elif BMI >= 24 and BMI < 27:
+	print('你的體重過重')
+	print('需要多加注意')
+elif BMI >= 27 and BMI < 30:
+	print('你的體重為輕度肥胖')
+	print('需要多加注意')
+elif BMI >= 30 and BMI < 35:
+	print('你的體重為中度肥胖')
+	print('需要多加注意')
+else:
+    print('你的體重為重度肥胖')
+    print('請立刻改善或就醫!')
